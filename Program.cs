85edbb77f575a5eb6772b42b9d08ca5045018bde
Garrett London <glondon@munichain.com>
2025-03-28 16:29:21 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+using LitRazor;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAntiforgery();
+
+app.MapRazorComponents<Routes>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
